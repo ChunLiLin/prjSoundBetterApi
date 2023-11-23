cc547d7794c1ba86dc7219c790b8ade95a5ba9f3
ChunLiLin <charles03292@gmail.com>
2023-11-23 16:10:29 +0800

    //

--- Controllers/apiTMembersController.cs
+++ Controllers/apiTMembersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -17,97 +18,55 @@ namespace prjSoundBetterApi.Controllers
         {
             _context = context;
         }
-
-        // GET: apiTMembers
-        public async Task<IActionResult> Index()
-        {
-            var dbSoundBetterContext = _context.TMembers.Include(t => t.FPermission);
-            return View(await dbSoundBetterContext.ToListAsync());
-        }
-        public IActionResult Indexjson()
+        //===List===
+        public IActionResult List()
         {
             var dbSoundBetterContext = _context.TMembers;
             return Json(dbSoundBetterContext);
         }
-
-        // GET: apiTMembers/Details/5
-        public async Task<IActionResult> Details(int? id)
+        //===搜尋===
+        public IActionResult QueryById(int? id)
         {
             if (id == null || _context.TMembers == null)
             {
                 return NotFound();
             }
 
-            var tMember = await _context.TMembers
+            var tMember = _context.TMembers
                 .Include(t => t.FPermission)
                 .FirstOrDefaultAsync(m => m.FMemberId == id);
             if (tMember == null)
             {
                 return NotFound();
             }
-
-            return View(tMember);
+            return Json(tMember);
         }
-
-        // GET: apiTMembers/Create
-        public IActionResult Create()
+        //===新增===
+        public IActionResult Create(TMember? tMember)
         {
-            ViewData["FPermissionId"] = new SelectList(_context.TMemberPromissions, "FPromissionId", "FPromissionId");
-            return View();
-        }
-
-        // POST: apiTMembers/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FMemberId,FUserame,FName,FPasswod,FPhone,FEmail,FGender,FBirthday,FCreationTime,FIntroduction,FPermissionId,FPhotoPath")] TMember tMember)
-        {
-            if (ModelState.IsValid)
+            if (tMember != null)
             {
                 _context.Add(tMember);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                _context.SaveChanges();
+                return Content("新增成功");
             }
-            ViewData["FPermissionId"] = new SelectList(_context.TMemberPromissions, "FPromissionId", "FPromissionId", tMember.FPermissionId);
-            return View(tMember);
+            return Content("錯誤");
         }
-
-        // GET: apiTMembers/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.TMembers == null)
-            {
-                return NotFound();
-            }
-
-            var tMember = await _context.TMembers.FindAsync(id);
-            if (tMember == null)
-            {
-                return NotFound();
-            }
-            ViewData["FPermissionId"] = new SelectList(_context.TMemberPromissions, "FPromissionId", "FPromissionId", tMember.FPermissionId);
-            return View(tMember);
-        }
-
-        // POST: apiTMembers/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("FMemberId,FUserame,FName,FPasswod,FPhone,FEmail,FGender,FBirthday,FCreationTime,FIntroduction,FPermissionId,FPhotoPath")] TMember tMember)
+        //===修改===
+        public IActionResult Edit(int id,TMember tMember)
         {
             if (id != tMember.FMemberId)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if (tMember != null)
             {
                 try
                 {
                     _context.Update(tMember);
-                    await _context.SaveChangesAsync();
+                    _context.SaveChanges();
+                    return Content("新增成功");
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -119,50 +78,200 @@ namespace prjSoundBetterApi.Controllers
                     {
                         throw;
                     }
-                }
-                return RedirectToAction(nameof(Index));
+                }               
             }
-            ViewData["FPermissionId"] = new SelectList(_context.TMemberPromissions, "FPromissionId", "FPromissionId", tMember.FPermissionId);
-            return View(tMember);
+            return Content("錯誤");
         }
-
-        // GET: apiTMembers/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.TMembers == null)
-            {
-                return NotFound();
-            }
-
-            var tMember = await _context.TMembers
-                .Include(t => t.FPermission)
-                .FirstOrDefaultAsync(m => m.FMemberId == id);
-            if (tMember == null)
-            {
-                return NotFound();
-            }
-
-            return View(tMember);
-        }
-
-        // POST: apiTMembers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        //===刪除===
+        public IActionResult Delete(int id)
         {
             if (_context.TMembers == null)
             {
                 return Problem("Entity set 'dbSoundBetterContext.TMembers'  is null.");
             }
-            var tMember = await _context.TMembers.FindAsync(id);
+            var tMember = _context.TMembers.Where(c => c.FMemberId == id).FirstOrDefault();
             if (tMember != null)
             {
                 _context.TMembers.Remove(tMember);
+                _context.SaveChanges();
+                return Content("刪除成功");
             }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Content("刪除失敗");
         }
+        //===Check===
+        public IActionResult checkUsername(string username)
+        {
+            if (username != null)
+            {
+                string feedback = "OK";
+                foreach (var item in _context.TMembers)
+                {
+                    if (item.FUserame == username)
+                    {
+                        feedback = "已存在的使用者名稱";
+                    }
+                }
+                return Content(feedback);               
+            }
+            return Content("請輸入使用名稱");
+        }
+        public IActionResult checkEmail(string email)
+        {
+            if (email != null)
+            {
+                string feedback = "OK";
+                foreach (var item in _context.TMembers)
+                {
+                    if (item.FUserame == email)
+                    {
+                        feedback = "電子郵件已被註冊";
+                    }
+                }
+                return Content(feedback);
+            }
+            return Content("請輸入電子郵件");
+        }
+        //// GET: apiTMembers1
+        //public async Task<IActionResult> Index()
+        //{
+        //    var dbSoundBetterContext = _context.TMembers.Include(t => t.FPermission);
+        //    return View(await dbSoundBetterContext.ToListAsync());
+        //}
+
+
+        //// GET: apiTMembers1/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null || _context.TMembers == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var tMember = await _context.TMembers
+        //        .Include(t => t.FPermission)
+        //        .FirstOrDefaultAsync(m => m.FMemberId == id);
+        //    if (tMember == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(tMember);
+        //}
+
+        // GET: apiTMembers1/Create
+        //public IActionResult Create()
+        //{
+        //    ViewData["FPermissionId"] = new SelectList(_context.TMemberPromissions, "FPromissionId", "FPromissionId");
+        //    return View();
+        //}
+
+        // POST: apiTMembers1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("FMemberId,FUserame,FName,FPasswod,FPhone,FEmail,FGender,FBirthday,FCreationTime,FIntroduction,FPermissionId,FPhotoPath")] TMember tMember)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(tMember);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["FPermissionId"] = new SelectList(_context.TMemberPromissions, "FPromissionId", "FPromissionId", tMember.FPermissionId);
+        //    return View(tMember);
+        //}
+
+        // GET: apiTMembers1/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null || _context.TMembers == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var tMember = await _context.TMembers.FindAsync(id);
+        //    if (tMember == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["FPermissionId"] = new SelectList(_context.TMemberPromissions, "FPromissionId", "FPromissionId", tMember.FPermissionId);
+        //    return View(tMember);
+        //}
+
+        // POST: apiTMembers1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("FMemberId,FUserame,FName,FPasswod,FPhone,FEmail,FGender,FBirthday,FCreationTime,FIntroduction,FPermissionId,FPhotoPath")] TMember tMember)
+        //{
+        //    if (id != tMember.FMemberId)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(tMember);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!TMemberExists(tMember.FMemberId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["FPermissionId"] = new SelectList(_context.TMemberPromissions, "FPromissionId", "FPromissionId", tMember.FPermissionId);
+        //    return View(tMember);
+        //}
+
+        // GET: apiTMembers1/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null || _context.TMembers == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var tMember = await _context.TMembers
+        //        .Include(t => t.FPermission)
+        //        .FirstOrDefaultAsync(m => m.FMemberId == id);
+        //    if (tMember == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(tMember);
+        //}
+
+        // POST: apiTMembers1/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    if (_context.TMembers == null)
+        //    {
+        //        return Problem("Entity set 'dbSoundBetterContext.TMembers'  is null.");
+        //    }
+        //    var tMember = await _context.TMembers.FindAsync(id);
+        //    if (tMember != null)
+        //    {
+        //        _context.TMembers.Remove(tMember);
+        //    }
+
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool TMemberExists(int id)
         {
